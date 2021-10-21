a0eb62dd562f92a131580a1a492787ac1d24d0ff
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2021-10-21 07:54:05 +0200

    Add more movieData to thumbnail to display later

--- src/pages/user/userDataParsers.ts
+++ src/pages/user/userDataParsers.ts
@@ -1,15 +1,17 @@
 import { MovieResult, MovieThumb, UserMovieList, MovieResponse } from './userDataTypes';
 
-// Add genres
 export const parseMovielistItem = (input: MovieResult ): MovieThumb => ({
     id: input.id,
     title: input.title,
     cover: input.poster_path !== null ? input.poster_path : '',
-    rating: input.rating ? input.rating : undefined
+    movieData: {
+        releaseDate: input.release_date,
+        genres: input.genre_ids,
+        rating: input.vote_average ? input.vote_average : undefined
+    }
 })
 
 export const parseMovielistRepsonse = (input: MovieResponse ): UserMovieList => { 
-    console.log('--> parseMovieResponse: ', input)
-    return input.results !== undefined ? input.results.map(parseMovielistItem) : [] 
 
+    return input.results !== undefined ? input.results.map(parseMovielistItem) : [] 
 }
